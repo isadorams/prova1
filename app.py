69a2b149c05d28a6a2c4de1e788de6c6eeeea85d
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-19 19:53:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 
 st.title('Relatórios Breast Cancer Wisconsin')
-#st.write("# Classificação quanto ao Câncer")
+st.write("## Classificação quanto ao Câncer")
 #st.write("## Breast Cancer Wisconsin")
-train = pd.read_csv('wdbc.csv')
-st.markdown('---')
-st.markdown('# Sobre a Base de Dados')
-#st.sidebar.write("### Parâmetros")
-#perimeter = st.sidebar.slider("Perimeter", 40.0, 190.0, 100.0)
-#area= st.sidebar.slider("Area", 184.0, 4254.0, 881.0)
-#compactness = st.sidebar.slider("Compactness", 0.01, 1.6, 0.25)
-#concavity = st.sidebar.slider("Concavity", 0.0, 1.30, 0.27)
+
+st.sidebar.write("### Parâmetros")
+perimeter = st.sidebar.slider("Perimeter", 40.0, 190.0, 100.0)
+area= st.sidebar.slider("Area", 184.0, 4254.0, 881.0)
+compactness = st.sidebar.slider("Compactness", 0.01, 1.6, 0.25)
+concavity = st.sidebar.slider("Concavity", 0.0, 1.30, 0.27)
 
     
-#with open("objetos.pkl", "rb") as arquivo:
-  #ss, classifier = pickle.load(arquivo)
+with open("objetos.pkl", "rb") as arquivo:
+  ss, classifier = pickle.load(arquivo)
 
-  #estrutura = {'perimeter': perimeter, 'area': area, 'compactness': compactness, 'concavity': concavity}
-  #df = pd.DataFrame(estrutura, index=[0])
+  estrutura = {'perimeter': perimeter, 'area': area, 'compactness': compactness, 'concavity': concavity}
+  df = pd.DataFrame(estrutura, index=[0])
  
-  #st.write("### Parâmetros de Entrada")
-  #st.write(df)
+  st.write("### Parâmetros de Entrada")
+  st.write(df)
   
   
   #df = ss.transform(df)
