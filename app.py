6386baa5faaf8db191e53a40662edc73db74d1a5
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-20 15:20:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,14 +24,14 @@ IMAGE_URL = "https://miro.medium.com/max/1400/1*51Hm0b9RlgnPVQLariliRw.png"
 st.image(IMAGE_URL, caption="Sunrise by the mountains")
 
 st.sidebar.write("### Parâmetros")
-radius = st.sidebar.slider("Radius", 7.8, 36, 16.3, 0.1)
-texture = st.sidebar.slider("Texture", 11, 50, 26, 0.1)
+radius = st.sidebar.slider("Radius", 7.8, 36.0, 6.3, 0.1)
+texture = st.sidebar.slider("Texture", 11.0, 50.0, 26.0, 0.1)
 perimeter = st.sidebar.slider("Perimeter", 40.0, 190.0, 100.0)
 area= st.sidebar.slider("Area", 184.0, 4254.0, 881.0)
 smoothness = st.sidebar.slider("Smoothness", 0.06, 0.22, 0.14, 0.1)
 compactness = st.sidebar.slider("Compactness", 0.01, 1.6, 0.25)
 concavity = st.sidebar.slider("Concavity", 0.0, 1.30, 0.27)
-concave = st.sidebar.slider("Concave", 0, 0.30, 0.11, 0.1)
+concave = st.sidebar.slider("Concave", 0.0, 0.30, 0.11, 0.1)
 symmetry = st.sidebar.slider("Symmetry", 0.14, 0.66, 0.30, 0.1)
 fractal = st.sidebar.slider("Fractal", 0.054, 0.20, 0.08, 0.1)
 
